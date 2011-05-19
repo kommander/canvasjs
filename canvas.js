dfc927480b0763309321a8ab8e049bce25c9a908
kommander <sebastian@formzoo.com>
2011-05-19 06:54:17 -0700

    Function.bind() for missing native implementations

---
+++ canvas.js
@@ -0,0 +1,15 @@
+/**
+ * canvas.js
+ * Basic functionality, reused by other components
+ */
+
+/**
+ * Bind a function to a specific context
+ */
+if(!Function.prototype.bind)
+  Function.prototype.bind= function(to){
+    var _self = this;
+    return function(){
+      return _self.apply((to) ? to : null, Array.prototype.slice.call(arguments));
+    }
+  };
