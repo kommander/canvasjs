432e672019c0c2bd995b5919246a5113f6d87d76
kommander <sebastian@formzoo.com>
2011-05-20 14:59:05 -0700

    many changes for the stars... useLines, width and height, set color, set sizes, set speed ...

--- g2d/objects/stars.js
+++ g2d/objects/stars.js
@@ -3,30 +3,60 @@
  * an Object for Loop.js 
  * Copyright 2011 Sebastian Herrlinger
  */
-kk.g2d.obj.Stars = function(maxStars, speed){
+kk.g2d.obj.Stars = function(width, height, maxStars, speed, color, min, max, timeout, useLines, lineScale){
     
-  var _maxStars = maxStars,
+  var _maxStars = maxStars || 0,
   _stars = [],
   _timeSecond = 0,
+  _starSize = null,
+  _lineLength = 0,
   
-  _createStars = function() {
+  /**
+   * Generates the star objects depending on the settings,
+   * is used to regenerate stars if a settings is changed
+   * or if stars are added.
+   */
+  _createStars = (function(from) {
     //generate stars
-    for(var i = _stars.length - 1; i < _maxStars; i++){
-      _stars.push({
-        x: kk.rand(0, 500),
-        y: kk.rand(0, 2000),
-        size: kk.rand(1, 4),
-        timeout: 0
-      });
+    for(var i = (from || 0); i < _maxStars; i++){
+      var size = kk.rand(_starSize[0], _starSize[1]);
+      var length = (this.useLines) ? this.lineScale * this.speed * size: size;
+      _stars[i]= {
+        size: size,
+        x: kk.rand(0, this.width),
+        y: kk.rand(-length, this.height + 1000),
+        timeout: (size < 0) ? kk.rand(0, this.timeout) : 0
+      };
     }
+  }).bind(this);
+  
+  //The time after which the stars appear again
+  this.timeout = timeout || 10000;
+  
+  //Width and height of the starfield
+  this.width = width || 500;
+  this.height = height || 1000;
+  
+  /**
+   * Set the minimum and maximum size of the stars,
+   * they get created randomly
+   */
+  this.starSize = function(min, max) {
+    _starSize = [min || 1, max || 4];
+    _createStars();
+    return this;
   };
   
-  _createStars();
+  this.starSize(min || 1, max || 4);
   
+  /**
+   * Changes the maximum stars that move over the screen,
+   * if needed generates or removes them
+   */
   this.maxStars = function(max){
     if(max > _maxStars){
       _maxStars = max;
-      _createStars();
+      _createStars(_stars.length - 1);
     } 
     else if(max) {
       _maxStars = max;
@@ -35,24 +65,45 @@ kk.g2d.obj.Stars = function(maxStars, speed){
     return _maxStars;
   };
   
-  this.speed = speed;
   
+  //If lines are used, the line length is determined by the speed and the star size,
+  //multiplied by the lineScale
+  this.useLines = useLines || false;
+  this.lineScale = lineScale || 0.1;
+  
+  //How fast are the stars moving in pixel per second
+  //The stars own speed is the general speed times their size
+  this.speed = speed || 10;
+  this.color = color || '#eee';
+  
+  /**
+   * Render the stars
+   */
   this.tick = function(context, timeDiff){
-    _timeSecond = timeDiff / 1000;
+    _timeSecond = timeDiff * 0.001;
+    context.fillStyle = this.color;
+    context.strokeStyle = this.color;
     for(var i = 0; i < _maxStars; i++){
+      _lineLength = (this.useLines) ? this.lineScale * this.speed * _stars[i].size: _stars[i].size;
       _stars[i].timeout -= timeDiff;
-      if((this.speed > 0 && _stars[i].y > context.canvas.height + _stars[i].size)
-        || (this.speed < 0 && _stars[i].y < _stars[i].size)){
-        _stars[i].y = this.speed > 0 ? -10 : context.canvas.height + 10;
-        _stars[i].x = kk.rand(10, context.canvas.width - 10);
-        _stars[i].timeout = kk.rand(0, 10000);
+      if((this.speed > 0 && _stars[i].y > this.height + _lineLength)
+        || (this.speed < 0 && _stars[i].y < _lineLength)){
+        _stars[i].y = this.speed > 0 ? -_stars[i].size : this.height + 10;
+        _stars[i].x = kk.rand(10, this.width - 10);
+        _stars[i].timeout = kk.rand(0, this.timeout);
       }
-      if(_stars[i].timeout <= 0){
+      else if(_stars[i].timeout <= 0){
         _stars[i].y += _timeSecond * this.speed * _stars[i].size;
         context.beginPath();
-        context.fillStyle = '#eee';
-        context.arc(_stars[i].x, _stars[i].y, _stars[i].size / 2, 0, Math.PI / 2 * 360, false);
-        context.fill();
+        if(this.useLines){
+          context.lineWidth = _stars[i].size;
+          context.moveTo(_stars[i].x, _stars[i].y - _lineLength);
+          context.lineTo(_stars[i].x, _stars[i].y);
+          context.stroke();
+        } else {
+          context.arc(_stars[i].x, _stars[i].y, _stars[i].size * 0.5, 0, Math.PI * 2, false);
+          context.fill();
+        }
         context.closePath();
       }
     }
